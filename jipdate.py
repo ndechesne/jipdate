ndechesne/jipdate

commit 49866d087fcc53b3ba169f97b545437faf2eee85

jipdate: avoid refetching issues during the parsing of the text file
During the parsing of the text file, we re-fetch each issue one by
one, and it can take a lot of time, when processing many issues at
once. We don't need to do that since we have already fetched all
issues when we generated the file. So this patch keeps track of
'my_issues' array and pass it around. In the parsing code, we just
need to locate the right issue object.

The parsing is significantly faster with this change.

Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

jipdate.py
@@ -240,7 +240,7 @@ def get_jira_issues(jira, username):
         f.write("\n")
 
     f.close()
-    return filename
+    return (filename, my_issues)
 
 
 def should_update():
@@ -262,7 +262,7 @@ def should_update():
             print("Incorrect input: %s" % answer)
 
 
-def parse_status_file(jira, filename):
+def parse_status_file(jira, filename, issues):
     """
     The main parsing function, which will decide what should go into the actual
     Jira call. This for example removes the beginning until it finds a
@@ -317,12 +317,11 @@ def parse_status_file(jira, filename):
             validissue = True
 
             try:
-                issue = jira.issue(myissue)
+                issue = [x for x in issues if str(x) == myissue][0]
                 issue_comments.append((issue, "", ""))
-            except  Exception as e:
-                if 'Issue Does Not Exist' in e.text:
-                    print('[{}] :  {}'.format(myissue, e.text))
-                    validissue = False
+            except IndexError as e:
+                print('[{}] :  {}'.format(myissue, "Issue not found"))
+                validissue = False
 
 
         # Stop parsing entirely.  This needs to be placed before regex_stop
@@ -500,7 +499,7 @@ def main(argv):
         sys.exit(os.EX_USAGE)
 
     if cfg.args.q:
-        filename = get_jira_issues(jira, username)
+        (filename, issues) = get_jira_issues(jira, username)
 
         if cfg.args.p:
             print_status_file(filename)
@@ -513,7 +512,7 @@ def main(argv):
 
     if get_editor():
         open_editor(filename)
-    parse_status_file(jira, filename)
+    parse_status_file(jira, filename, issues)
 
 if __name__ == "__main__":
     main(sys.argv)

commit b6ec4f322d23242611ab7d45286ac440da1ebfa0

jipdate: fix regression with file operation
A regression was found in
49866d0 jipdate: avoid refetching issues during the parsing of the text file

This patch introduced an optimization when using a user query
(-q). However when using file mode, we don't run a Jira query in the
initial phase, so once we do the parsing, the issues have not been
fetched yet.

This patch fixes this situation, so now we do the following:

1. we check if we have fetched issues already
2. if so, we check if the issue we are looking for exist in the
already fetched set
3. otherwise, we query Jira

Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

jipdate.py
@@ -316,13 +316,22 @@ def parse_status_file(jira, filename, issues):
             myissue = match.group(1)
             validissue = True
 
+            # if we ran a query, we might already have fetched the issue
+            # let's try to find the issue there first, otherwise ask Jira
             try:
                 issue = [x for x in issues if str(x) == myissue][0]
                 issue_comments.append((issue, "", ""))
-            except IndexError as e:
-                print('[{}] :  {}'.format(myissue, "Issue not found"))
-                validissue = False
 
+            # We had fetched already, but issue is not found
+            # or we haven't queried Jira yet, at all
+            except (IndexError, TypeError) as e:
+                try:
+                    issue = jira.issue(myissue)
+                    issue_comments.append((issue, "", ""))
+                except  Exception as e:
+                    if 'Issue Does Not Exist' in e.text:
+                        print('[{}] :  {}'.format(myissue, e.text))
+                        validissue = False
 
         # Stop parsing entirely.  This needs to be placed before regex_stop
         # or the .* will match and [FIN] won't be processed
@@ -512,7 +521,15 @@ def main(argv):
 
     if get_editor():
         open_editor(filename)
-    parse_status_file(jira, filename, issues)
+
+    # At this point, we might have fetches some issues already, if so, let's use them to
+    # avoid fetching them again later.
+    try:
+        issues
+    except NameError:
+        parse_status_file(jira, filename, None)
+    else:
+        parse_status_file(jira, filename, issues)
 
 if __name__ == "__main__":
     main(sys.argv)